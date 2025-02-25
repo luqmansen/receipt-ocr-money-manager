ffa1d981cefa9bffc945f95a42747277e033a4f8
luqmansen <luqmansen@gmail.com>
2025-02-25 21:39:14 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
+import {defineConfig} from 'vite';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default defineConfig({
+    plugins: [
+        commonjs(),
+    ],
+});
+
 module.exports = {
-  root: '.',
-  build: {
-    outDir: 'docs', // to allow GitHub Pages to serve the site
-    emptyOutDir: true, // also necessary
-  }
+    root: '.',
+    build: {
+        outDir: 'docs', // to allow GitHub Pages to serve the site
+        emptyOutDir: true,
+        minify: false, // for debugging
+    }
 }
 
